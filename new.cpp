96b1809db1d26d23958dec33c787592bba27f05a
Samyak P Shetty <114393522+samyakpshetty@users.noreply.github.com>
2023-02-13 20:06:55 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,11 @@
+//File: new.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello there, I'am Samyak\n";
+  cout << "My SRN is PES2UG20CS307\n";
+  cout << "Iam from Section E\n";
+  cout << "This code is for Task 5 of Jenkins!!\n";
+  return 0;
+}
